fd1a9f66325b0b3442b6ebecc43f41be9052af0f
Gauri Mahashabde <gauri2interact@gmail.com>
2022-11-11 14:40:29 +0530

    formatted

--- js_intro/script.js
+++ js_intro/script.js
@@ -1,16 +1,16 @@
 //Declaring a variable
-var name ;
+var name;
 //Initializing a varibale
 //using var key word
 var name1 = "Ross";
-document.getElementById("var").innerHTML=name1;
+document.getElementById("var").innerHTML = name1;
 console.log(name1);
 //using let key word
 let name1 = "Ross";
-document.getElementById("let").innerHTML=name1;
+document.getElementById("let").innerHTML = name1;
 console.log(name1);
 //using const key word
 const name1 = "Ross";
-document.getElementById("const").innerHTML=name1;
+document.getElementById("const").innerHTML = name1;
 console.log(name1);
 
