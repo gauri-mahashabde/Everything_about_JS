4d2903a085451da43549b2df46283fea0d103a8a
Gauri Mahashabde <gauri2interact@gmail.com>
2022-11-20 23:04:31 +0530

    example 2 for recursion

--- js-recursion/script.js
+++ js-recursion/script.js
@@ -9,3 +9,17 @@ function factorial(x) {
 }
 
 console.log(factorial(5));
+
+//fibonacci number
+//0,1,1,2,3,5,8,13,21,34
+
+function fibonacci(n) {
+    if (n == 1) return 0;
+    if (n == 2) return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+const result = fibonacci(5);
+console.log(result);
+
