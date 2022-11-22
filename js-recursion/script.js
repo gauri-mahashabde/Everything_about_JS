6dbf4721cd58f61d2ffb05730a9de71605168b44
Gauri Mahashabde <gauri2interact@gmail.com>
2022-11-22 20:15:04 +0530

    Recursion example added

--- js-recursion/script.js
+++ js-recursion/script.js
@@ -8,7 +8,7 @@ function factorial(x) {
     return x * factorial(x - 1);
 }
 
-console.log(factorial(5));
+//console.log(factorial(5));
 
 //fibonacci number
 //0,1,1,2,3,5,8,13,21,34
@@ -21,24 +21,51 @@ function fibonacci(n) {
 }
 
 const result = fibonacci(5);
-console.log(result);
+//console.log(result);
 
 
 //example 3 reverse a string
 const str = "Virat"
 function reverseString(str) {
-    
-    if( str == undefined || str == null || str == "") return "";
+
+    if (str == undefined || str == null || str == "") return "";
     return reverseString(str.substr(1)) + str[0];
 }
 
 const rvstr = reverseString(str);
-console.log(rvstr);
+//console.log(rvstr);
 
 
 
 const str2 = "Anushka"
 const rvstr2 = reverseString(str2);
-console.log(rvstr2);
+//console.log(rvstr2);
+
+//example 4 
+
+const arr = [5, 7, 8, 3, 2, 1];
+
+function sumofodd(i) {
+
+    if (i <= 0) {
+        return 0;
+    }
+return i + sumofodd(i - 2)
+
+}
 
 
+
+function getSum(i) {
+    if (i % 2 == 0) {
+        return sumofodd(i - 1)
+
+    }
+    else {
+        return sumofodd(i);
+    }
+}
+
+getSum(7); 7 + 5 ,5 + 3 ,3 + 1, 1 + 0 = 16;
+
+getSum(8);
