2f82ca1c951c1c8bd4a6894ea84846d51d06a6c1
Gauri Mahashabde <gauri2interact@gmail.com>
2022-11-23 18:26:14 +0530

    Accessing array elements one by one

---
+++ js-array/array.js
@@ -0,0 +1,9 @@
+//Ways of iterating over a array in JavaScript
+
+ // Accessing array elements one by one
+ const array = [ 'geeks', '4', 'geeks' ];
+    console.log(array[0]);
+    console.log(array[1]);
+    console.log(array[2]);
+
+    
