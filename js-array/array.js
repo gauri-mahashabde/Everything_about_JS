f17c6f5ec2e76d7dbe652d03e7e5099190add46e
Gauri Mahashabde <gauri2interact@gmail.com>
2022-11-23 21:45:56 +0530

    iterating over a array using for and while loop

--- js-array/array.js
+++ js-array/array.js
@@ -6,4 +6,19 @@
     console.log(array[1]);
     console.log(array[2]);
 
-    
+//Using for loop
+
+ const array1 = [ 1, 2, 3, 4, 5, 6 ];
+ 
+ for(let i = 0; i < array1.length;i++){
+    console.log(array1[i]);
+ }
+
+ //Using while loop
+
+ let i = 0;
+ while(i<array1.length){
+    console.log(array1[i]);
+    i++
+
+ }
