436933375edc033683cf8f01630b023d49c54c41
Gauri Mahashabde <gauri2interact@gmail.com>
2022-12-07 11:21:10 +0530

    Accessing elements Using DOM

---
+++ js-dom/dom1.js
@@ -0,0 +1,38 @@
+/*
+element selector
+single element selector
+multiple element selector*/
+
+//single element selector
+let element1 = document.getElementById('heading');
+//element1 = element1.id;
+//element1 = element1.childNodes;
+//element1 = element1.parentNode;
+//element1.style.color = "red";
+//element1.innerText = "Hello world";
+//element1.innerHTML = "<i>Hello World</i>";
+//console.log(element1);
+
+let element2 = document.querySelector('div');
+//element2.style.color = 'green';
+//console.log(element2);
+
+//multiple element selector
+let element3 = document.getElementsByClassName('child');
+//Array.from(element3).forEach(element3 => {console.log(element3);
+//element3.style.color = 'blue'})
+//element3.style.color = "pink";
+//console.log(element3);
+
+let element4 = document.querySelectorAll('div');
+//Array.from(element4).forEach(element4 => {console.log(element4);
+    //element4.style.color = 'yellow'})
+    //element4.style.color = "blue";
+//console.log(element4);
+
+
+let b = document;
+//b = document.forms;
+console.log(b);
+
+
