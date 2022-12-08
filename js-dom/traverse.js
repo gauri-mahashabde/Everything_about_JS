0f3572dbfe7210a2216aa1ed7d2b43cacd1ee793
Gauri Mahashabde <gauri2interact@gmail.com>
2022-12-08 20:21:18 +0530

    Dom Manipulation

---
+++ js-dom/traverse.js
@@ -0,0 +1,29 @@
+
+const gp = document.querySelector('.grandparent');
+//console.log(gp);
+
+const par = document.querySelector('.parent');
+//console.log(par);
+
+const ch = document.querySelector('.child');
+//console.log(ch);
+
+const gp1 = document.querySelectorAll('.grandparent');
+//console.log(gp1);
+
+const par1 = document.querySelectorAll('.parent');
+//console.log(par1);
+
+const ch1 = document.querySelectorAll('.child');
+//console.log(ch1);
+
+//const grandparent = document.querySelector('.grandparent').lastElementChild;
+//console.log(grandparent);
+//const parent = document.querySelector('.parent').lastElementChild;
+//console.log(parent);
+//const parent2 = document.querySelectorAll('.parent');
+    //console.log(parent2) 
+
+
+
+
