5826b8294a0cac1386faf248656eb31a9fcde5ea
Gauri Mahashabde <gauri2interact@gmail.com>
2022-12-09 12:44:58 +0530

    Examples of for and while Loop

--- js-loops/whileloop.js
+++ js-loops/whileloop.js
@@ -1,4 +1,4 @@
-//print all the even numbers from the range of 0-10
+/*print all the even numbers from the range of 0-10
 let count = 0;
 while (count < 11) {
     // console.log(count);
@@ -10,6 +10,38 @@ while (count < 11) {
 let i = 1;
 while (i < 5) {
     if (i % 2 != 0)
-        console.log(i);
+        //console.log(i);
     i++;
 }
+
+//display a sum of all the natural numbers in the range of 0-10
+let j = 0;
+//let sum = 0;
+let sum2 = 0;
+while (j < 11) {
+    
+    sum2 = sum2 + j;
+    // console.log(sum2 , j);
+    j++
+
+}
+
+//check odd and even numbers of an array and return 2 new array
+const arr12 = [3, 6, 1, 5, 9, 4, 7, 10, 11];
+const evenNumbers1 = [];
+const oddNumbers1 = [];
+
+/*let k = 0;
+while (k< arr12.length){
+    if (arr12[i] % 2 == 0) {
+        evenNumbers1.push(arr12[k])
+    }
+    else {
+        oddNumbers1.push(arr12[k])
+    }
+    k++;
+
+}
+//console.log(arr12);
+console.log(evenNumbers1);
+console.log(oddNumbers1);*/
