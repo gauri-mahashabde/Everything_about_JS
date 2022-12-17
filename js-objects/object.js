3488c6add865129146bcca328ef8640453e33dae
Gauri Mahashabde <gauri2interact@gmail.com>
2022-12-17 22:26:08 +0530

    Objects in Js

---
+++ js-objects/object.js
@@ -0,0 +1,7 @@
+//JavaScript object is a non-primitive data-type that allows you to store multiple collections of data.
+
+const names = {
+    firstName: "peter",
+    middleaName: "mark",
+    lastName: "ben",
+  };
