abb6beab8eb815298ceb72fbcb3ba77d067b7487
Gauri Mahashabde <gauri2interact@gmail.com>
2022-12-19 21:54:28 +0530

    Adding new element in map

--- js-map/map.js
+++ js-map/map.js
@@ -4,3 +4,15 @@
 const map = new Map();
 
 console.log(map);
+
+const cars = new Map([["car1","BMW"],["car2","Benz"],["car3","Mercedes"]]);
+
+console.log(cars);
+
+const students = new Map();
+
+students.set('Ananya','101');
+students.set('Bhagya','102');
+students.set('Chaitra','103');
+
+console.log(students);
