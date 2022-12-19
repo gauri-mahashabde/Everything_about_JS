73df44d67253ab0f5c62d3018bcf26e9dce801e0
Gauri Mahashabde <gauri2interact@gmail.com>
2022-12-19 21:52:43 +0530

    Map creation in JS

---
+++ js-map/map.js
@@ -0,0 +1,6 @@
+//Map is a collection of key-value pairs where the key can be of any type
+//Map remembers the original order in which the elements were added to it, 
+
+const map = new Map();
+
+console.log(map);
