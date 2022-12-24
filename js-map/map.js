31728bae1ec33eed1be374de010ed0233cfae687
Gauri Mahashabde <gauri2interact@gmail.com>
2022-12-24 22:31:59 +0530

    Convert a map into an Array

--- js-map/map.js
+++ js-map/map.js
@@ -92,3 +92,15 @@ console.log(ageMap.entries());
 
   const addressobj = Object.fromEntries(addressMap);
   console.log(addressobj);
+
+  //convert a map into an array
+  const food = new Map();
+  food.set('milk', 200);
+  food.set("tea", 300);
+  food.set('coffee', 500);
+
+  console.log(food);
+  
+  console.log(Array.from(food));
+
+  console.log([...food]);
