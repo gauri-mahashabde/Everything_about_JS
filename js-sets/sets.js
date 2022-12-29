fbc7d94ad84ce39035e2493bbbf5324ccdfff842
Gauri Mahashabde <gauri2interact@gmail.com>
2022-12-29 18:35:38 +0530

    Set

--- js-sets/sets.js
+++ js-sets/sets.js
@@ -21,3 +21,37 @@ saladSet.add('🥒');
 //console.log(saladSet);
 
 saladSet.add('🥒')
+
+//console.log(saladSet.has('🥕'));
+//console.log(saladSet.has('🥦'));
+
+saladSet.delete('🥑');
+//console.log('I do not like 🥑, remove from the salad:', saladSet);
+
+saladSet.clear();
+//console.log(saladSet);
+
+const houseNos = new Set([360, 567, 101]);
+
+// Get the SetIterator using the `values()` method
+//console.log(houseNos.values());
+
+//creation 
+
+let employee = new Set();
+console.log(employee);
+
+
+employee.add('1');
+employee.add('1');
+employee.add('2');
+employee.add('3');
+
+console.log(employee);
+
+let age = new Set();
+
+age.add([{name1:"Gauri"}]);
+age.add(["joye",35]);
+
+console.log(age);
