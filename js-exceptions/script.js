2d43c9b282e589b98ffc71f1b2bcdcf6252a726d
Gauri Mahashabde <gauri2interact@gmail.com>
2023-01-02 19:49:53 +0530

    Try Catch

--- js-exceptions/script.js
+++ js-exceptions/script.js
@@ -9,3 +9,23 @@ try {
     console.log(error.name);
     console.log(error.message);
 }
+
+try {
+    myName();
+} catch (error) {
+   if (error instanceof SyntaxError)
+   {
+console.log("This is a syntax error");
+   }else if (error instanceof ReferenceError)
+   {
+   console.log("This is a reference error");
+   }
+   else if (error instanceof TypeError)
+   {
+   console.log("This is a type error");
+   }
+
+    else {
+        console.log("This is a Unknown error");
+    }
+}
