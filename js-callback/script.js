99600e9eed5888eb651d9be011c6930fafbbfb72
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-21 19:15:50 +0530

    call back example added

--- js-callback/script.js
+++ js-callback/script.js
@@ -1,15 +1,30 @@
-function fetchData(url , callback){
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener("readystatechange",()=>{
-        if(xhr.readyState === 4 && xhr.status === 200){
-            callback(xhr.responseText)
-        }
-    })
+//simple example of callback function
 
-    xhr.open("GET",url);
-    xhr.send();
+function sayHello(name, callback) {
+  console.log(`Hello, ${name}!`);
+  callback();
 }
 
-fetchData("https://jsonplaceholder.typicode.com/posts/1",response => {
-    console.log(response);
+function goodbye() {
+  console.log("Goodbye!");
+}
+
+sayHello("John", goodbye);
+
+//real time example of Callback
+
+function fetchData(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(xhr.responseText);
+    }
   });
+
+  xhr.open("GET", url);
+  xhr.send();
+}
+
+fetchData("https://jsonplaceholder.typicode.com/posts/1", (response) => {
+  console.log(response);
+});
