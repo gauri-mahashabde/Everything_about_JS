94df1d252077fb7a8989e53604d3ae480f8ba053
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-21 19:21:43 +0530

    Promise example added

--- js-promise/script.js
+++ js-promise/script.js
@@ -1,32 +1,36 @@
 //syntax of promise
 
-new Promise(function(resolve, reject) {
+new Promise(function (resolve, reject) {
   // Some asynchronous operation
 });
 
+//simple example for promise
+const myPromise = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve("The operation completed successfully!");
+  }, 2000);
+});
 
-
-
-
+//real time example for promise
 function fetchData(url) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-  
-      xhr.addEventListener("readystatechange", () => {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            resolve(xhr.responseText);
-          } else {
-            reject("Error fetching data");
-          }
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(xhr.responseText);
+        } else {
+          reject("Error fetching data");
         }
-      });
-  
-      xhr.open("GET", url);
-      xhr.send();
+      }
     });
-  }
-  
-  fetchData("https://jsonplaceholder.typicode.com/posts/1")
-    .then(response => console.log(response))
-    .catch(error => console.error(error));
+
+    xhr.open("GET", url);
+    xhr.send();
+  });
+}
+
+fetchData("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error));
