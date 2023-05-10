460c9f78d0a69abf1211e10e9b2626d1bda1d266
Gauri Mahashabde <gauri2interact@gmail.com>
2023-05-10 22:57:52 +0530

    Promise expalined

--- js-promise/script2.js
+++ js-promise/script2.js
@@ -3,10 +3,24 @@
 var promise = new Promise(function(resolve, reject) {
     // do thing, then…
   
-    if (/* everything worked */) {
+    if (1==1) {
       resolve("See, it worked!");
     }
     else {
       reject(Error("It broke"));
     }
   });
+
+  //example
+
+  const count = true;
+
+let countValue = new Promise(function (resolve, reject) {
+    if (count) {
+        resolve("There is a count value.");
+    } else {
+        reject("There is no count value");
+    }
+});
+
+console.log(countValue);
