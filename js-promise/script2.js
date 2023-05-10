b20e70ffb5338d2e32ac6fa2a57370782c3ecb45
Gauri Mahashabde <gauri2interact@gmail.com>
2023-05-10 22:53:06 +0530

    Promise expalined

---
+++ js-promise/script2.js
@@ -0,0 +1,12 @@
+//syntax
+
+var promise = new Promise(function(resolve, reject) {
+    // do thing, then…
+  
+    if (/* everything worked */) {
+      resolve("See, it worked!");
+    }
+    else {
+      reject(Error("It broke"));
+    }
+  });
