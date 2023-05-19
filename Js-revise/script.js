cbfb4a45cff95c7288497f31d52af801f42ae44d
Gauri Mahashabde <gauri2interact@gmail.com>
2023-05-19 19:25:58 +0530

    obj added

--- Js-revise/script.js
+++ Js-revise/script.js
@@ -173,3 +173,17 @@ var obj = {
 }
  
 obj.getName();
+
+
+var obj = {
+  name:  "vivek",
+  getName: function(){
+  console.log(this.name);
+}
+   
+}
+     
+var getName = obj.getName;
+     
+var obj2 = {name:"akshay", getName };
+obj2.getName();
