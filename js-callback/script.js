8306edfb91f1f7cf7faa42b1a3a739a79bca7075
Gauri Mahashabde <gauri2interact@gmail.com>
2023-05-21 20:55:30 +0530

    callback example added

--- js-callback/script.js
+++ js-callback/script.js
@@ -28,3 +28,18 @@ function fetchData(url, callback) {
 fetchData("https://jsonplaceholder.typicode.com/posts/1", (response) => {
   console.log(response);
 });
+
+//example 
+// function
+function greet(name, callback) {
+  console.log('Hi' + ' ' + name);
+  callback();
+}
+
+// callback function
+function callMe() {
+  console.log('I am callback function');
+}
+
+// passing function as an argument
+greet('Peter', callMe);
