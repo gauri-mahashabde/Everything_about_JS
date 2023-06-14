6041c349d2e67dad3465b5075d98c8334146753a
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-14 21:11:48 +0530

    event explained

--- js_events/eventnew.js
+++ js_events/eventnew.js
@@ -1,1 +1,11 @@
-//sname_of_EventHandler = "The javaScript code / function which is required to be executed"
+//name_of_EventHandler = "The javaScript code / function which is required to be executed"
+
+<input type="button" value="Save" onclick="alert('The button is clicked!')"/>
+
+    function myAlert() {
+        alert('The button is clicked!');
+    }
+
+<input type="button" value="Save" onclick="myAlert()"/>
+
+
