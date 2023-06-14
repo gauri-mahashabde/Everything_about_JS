56889033efdf875939324f60eb3bfb4b432850d7
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-14 21:12:51 +0530

    event explained

--- js_events/eventnew.js
+++ js_events/eventnew.js
@@ -9,3 +9,13 @@
 <input type="button" value="Save" onclick="myAlert()"/>
 
 
+let btn = document.querySelector("button");
+btn.addEventListener("mousedown", event => {
+  if (event.btn == 0) {
+    console.log("Left mouse button was pressed");
+  } else if (event.btn == 1) {
+    console.log("Middle mouse button was pressed");
+  } else if (event.btn == 2) {
+    console.log("Right mouse button was pressed");
+  }
+});
