6e78b8339a72a3fae4fdc320d4cc14a0b7e6573f
Gauri Mahashabde <gauri2interact@gmail.com>
2023-07-04 20:58:26 +0530

    sets in js

--- js-sets/sets.js
+++ js-sets/sets.js
@@ -55,3 +55,12 @@ age.add([{name1:"Gauri"}]);
 age.add(["joye",35]);
 
 console.log(age);
+
+
+// create Set
+const set1 = new Set(); // an empty set
+console.log(set1); // Set {}
+
+// Set with multiple types of value
+const set2 = new Set([1, 'hello', {count : true}]);
+console.log(set2); // Set {1, "hello", {count: true}}
